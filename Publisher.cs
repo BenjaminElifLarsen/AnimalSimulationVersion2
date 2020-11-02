21eb79832c071f0ab5a59c9b68eaee658d51e5e9
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-02 07:41:17 +0100

    moved the location of a property

--- Publisher.cs
+++ Publisher.cs
@@ -7,10 +7,10 @@ namespace AnimalSimulationVersion2
     class Publisher
     {
         private static AnimalPublisher animalPublisher;
+        public static AnimalPublisher GetAnimalInstance { get => animalPublisher; }
         static Publisher()
         {
             animalPublisher = new AnimalPublisher();
         }
-        public static AnimalPublisher GetAnimalInstance {get => animalPublisher;}
     }
 }
