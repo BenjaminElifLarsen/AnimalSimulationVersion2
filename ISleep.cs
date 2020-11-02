6dea1e28b1376a916c0166c13be69b4e2da90a4c
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-02 14:56:15 +0100

    added new properties and altered Active to be only get

--- ISleep.cs
+++ ISleep.cs
@@ -6,9 +6,14 @@ namespace AnimalSimulationVersion2
 {
     interface ISleep
     { //not all animals sleep fully, some species only sleep with one part of the brain at a time.
+        public abstract bool Sleeping { get; set; }
+        public abstract float TimeSlept { get; set; }
+        public abstract float SleepLength { get; }
         public abstract float EnergyLevel { get; set; } //affects change to fail/succede at something like losing a prey or get away
-        public abstract string Active { get; set; } //dayactive, nightactive or both
+        public abstract string Active { get;} //dayactive, nightactive or both
+        public abstract float MaxEnergyLevel { get;}
         public bool IsActive(string period, string activePeriod);
         public void Sleep();
+        
     }
 }
