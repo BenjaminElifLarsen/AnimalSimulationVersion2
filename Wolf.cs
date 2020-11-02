49dd7ad84989fea371a9651591ad0285dd6d475e
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-02 14:58:51 +0100

    added the changes from ISleep and ITerriotorial, updated AI, added implementation for GenerateTerritory, Move and Sleep. Minor changes here and there

--- Wolf.cs
+++ Wolf.cs
@@ -15,14 +15,17 @@ namespace AnimalSimulationVersion2
         /// How long time the current pregnacy has lasted in seconds.
         /// </summary>
         protected float periodInPregnacy;
-        protected (char Gender, byte Weight)[] genderInformation = new (char Gender, byte Weight)[] { ('f',50),('m',50) };
 
         public override float AttackRange { get; set; }
         public string[] Targets { get; set; }
-        public (int x, int y)[] Territory { get; set; }
-        public string Active { get; set; }
+        public (ushort x, ushort y)[] Territory { get; set; }
+        public string Active { get; }
         public float EnergyLevel { get; set; }
         public override float AttackSpeedMultiplier { get; set; }
+        public float MaxEnergyLevel { get;}
+        public float TimeSlept { get; set; }
+        public float SleepLength { get; }
+        public bool Sleeping { get; set; }
 
         public Wolf(string species, int reproductionAge, (float X, float Y) location, float maxAge, (byte Minimum, byte Maximum) birthAmount, float movementSpeed, float hunger, Point[] design, (int Red, int Green, int Blue) colour, string[] foodSource, float nutrienceValue, IHelper helper, AnimalPublisher animalPublisher, DrawPublisher drawPublisher) : base(species, reproductionAge, location, maxAge, birthAmount, movementSpeed, hunger, design, colour, foodSource, nutrienceValue, helper, animalPublisher, drawPublisher)
         {
@@ -32,9 +35,9 @@ namespace AnimalSimulationVersion2
             AttackRange = 20;
             AttackSpeedMultiplier = 1.5f;
             lengthOfPregnacy = 9;
-
+            genderInformation = new (char Gender, byte Weight)[] { ('f', 50), ('m', 50) };
+            MaxEnergyLevel = 300;
         }
-
 
         public void AttackOther(string ID)
         { //transmit out a delegate
@@ -47,36 +50,49 @@ namespace AnimalSimulationVersion2
                 Death();
             else
             {
-                if((Hunger < 50 && EnergyLevel > 0) || Hunger < 10) //softcode those values later.
+                if(periodInPregnacy < lengthOfPregnacy && HasMated)
+                    periodInPregnacy += timeSinceLastUpdate;
+                if (!Sleeping)
                 {
-                    if(foodID == null) //if wolves do not mate for life, maybe discard the mateID if not null if it is hungry
-                        foodID = FindFood();
-                    if (foodID != null)
+                    if ((Hunger < 50 && EnergyLevel > 0) || Hunger < 10) //softcode those values later.
                     {
-                        TrackPrey();
-                        Move();
-                        AttackPrey();
-                    }
-                    else
-                        Move();
-                }else if (Age >= ReproductionAge && EnergyLevel > 0)
-                {
-                    if(TimeToReproductionNeed <= 0)
-                    {
-                        if(mateID == null)
-                            mateID = FindMate();
-                        if (mateID != null)
+                        if (foodID == null) //if wolves do not mate for life, maybe discard the mateID if not null if it is hungry
+                            foodID = FindFood();
+                        if (foodID != null)
                         {
-                            MateLocation = GetMateLocation(mateID);
+                            TrackPrey();
                             Move();
-                            Mate();
+                            AttackPrey();
                         }
                         else
                             Move();
                     }
-                }else if (EnergyLevel <= 0)
+                    else if (Age >= ReproductionAge && EnergyLevel > 0)
+                    {
+                        if (TimeToReproductionNeed <= 0)
+                        {
+                            if (mateID == null)
+                                mateID = FindMate();
+                            if (mateID != null)
+                            {
+                                MateLocation = GetMateLocation(mateID);
+                                Move();
+                                Mate();
+                            }
+                            else
+                                Move();
+                        }
+                    }
+                    else if (EnergyLevel <= 0)
+                    {
+                        Sleep(); 
+                    }
+                }
+                else
                 {
-                    Sleep(); //would still need to update the pregnacy time in this case and when hungry
+                    TimeSlept += timeSinceLastUpdate; //consider making a method in ISleep for this
+                    if (TimeSlept >= SleepLength) //maybe allow for an ealy wake up if it is to hungry
+                        Sleeping = false;
                 }
 
             }
@@ -110,9 +126,20 @@ namespace AnimalSimulationVersion2
         {
             throw new NotImplementedException();
         }
-        public (int x, int y)[] GenerateTerritory()
+        public (ushort X, ushort Y)[] GenerateTerritory()
         {
-            throw new NotImplementedException();
+            byte maxLength = 200;
+            (ushort X, ushort Y)[] corners = new (ushort X, ushort Y)[4];
+            (ushort width, ushort height) mapSize = mapInformation.GetSizeOfMap;
+            ushort mostLeftValue = Location.X - maxLength < 0 ? (ushort)0 : (ushort)(Location.X - maxLength); //ensures that the wolf will spawns its territory inside near itself
+            ushort mostTopValue = Location.Y - maxLength < 0 ? (ushort)0 : (ushort)(Location.Y - maxLength);
+            ushort leftX = (ushort)helper.GenerateRandomNumber(mostLeftValue, mapSize.width - maxLength);
+            ushort topY = (ushort)helper.GenerateRandomNumber(mostTopValue, mapSize.height - maxLength);
+            corners[0] = (leftX, topY); //left top
+            corners[1] = ((ushort)helper.GenerateRandomNumber(leftX+10,leftX+100),(ushort)helper.GenerateRandomNumber(topY,topY+100)); //right top
+            corners[2] = ((ushort)helper.GenerateRandomNumber(corners[1].X - 6, corners[1].X + 50), (ushort)helper.GenerateRandomNumber(corners[1].Y, corners[1].Y + 50)); //right bottom 
+            corners[3] = ((ushort)helper.GenerateRandomNumber(corners[2].X - 3, corners[2].X + 30), (ushort)helper.GenerateRandomNumber(corners[2].Y+10, corners[2].Y + 80)); //left bottom
+            return corners;
         }
 
         public bool IsActive(string period, string activePeriod)
@@ -122,8 +149,7 @@ namespace AnimalSimulationVersion2
 
         protected override void Eat()
         {
-            Hunger = 100; //the nutriedjwdwpd value.
-            throw new NotImplementedException();
+            Hunger = animalPublisher.Eat(foodID);
         }
 
         /// <summary>
@@ -138,35 +164,38 @@ namespace AnimalSimulationVersion2
             }
             if(HasMated)
             { 
-                //either here or before the call, check if the end location and current location is the same.
                 if(Gender == 'f') //later on, alter the FindMate() to check if the wolf is alpha, if a pack wolf, and only mate with the other alpha. 
                 { //maybe have both parents stay together for a while while the female wolf is pregnant. Check up if both parents stay with their children, also if they mate is for life
-                    periodInPregnacy += timeSinceLastUpdate;
                     if(periodInPregnacy >= lengthOfPregnacy)
                     { //generate a random number, need to depedency inject a random generator to ensure the values are not the same for each call if multiple calls happen quickly. Also needed for random movement.
-                        byte childAmount = 0; //seems like wolves mate for life, but if losing a mate, they will quickly find another one.
+                        byte childAmount = (byte)helper.GenerateRandomNumber(BirthAmount.Minimum, BirthAmount.Maximum); //seems like wolves mate for life, but if losing a mate, they will quickly find another one.
                         for (int i = 0; i < childAmount; i++)
                             new Wolf(Species, ReproductionAge, Location, MaxAge, BirthAmount, MovementSpeed, Hunger, Design, Colour, FoodSource, NutrienValue, helper, animalPublisher, drawPublisher);
                         TimeToReproductionNeed = 200; //keep the new wol(f/ves) in a list for a short period so the IPack methods can be updated to contain the newest family.
                         HasMated = false;
                     }
                 }
-                else //how to let the father now of the children.
+                else //how to let the father now of the children. Maybe the female should raise an event to let the father know of the children
                 {
-                    periodInPregnacy += timeSinceLastUpdate;
-                    if (periodInPregnacy >= lengthOfPregnacy)
-                    {
-                        TimeToReproductionNeed = 200;
-                        HasMated = false;
-                    }
+                    TimeToReproductionNeed = 200 + lengthOfPregnacy;
+                    HasMated = false;
                 }
             }
             throw new NotImplementedException();
         }
 
-        protected override void Move() //needs to check if the wolf is hungry and go after it, if it is not go for a mate if it can mate else select a random location and go to it
-        { //if it goes for a prey, it should call TrackPrey
-            throw new NotImplementedException();
+        protected override void Move() //maybe move this up to Animalia
+        {
+            float distanceToEndLocation = Math.Abs(MoveTo.X - Location.X) + Math.Abs(MoveTo.Y - Location.Y);
+            //calculates the %s of the move distance that belong to x and y and then multiply those numbers with the current movement speed. 
+            float xPercentage = Math.Abs(MoveTo.X - Location.X) / distanceToEndLocation;
+            float xCurrentSpeed = xPercentage * CurrentMovementSpeed;
+            float yCurrentSpeed = (1 - xPercentage) * CurrentMovementSpeed;
+            //need to find the direction to move in. 
+            float amountOfXToMove = (MoveTo.X - Location.X) * xCurrentSpeed;
+            float amountOfYToMove = (MoveTo.Y - Location.Y) * (yCurrentSpeed);
+            //set the new location
+            Location = (Location.X + amountOfXToMove, Location.Y + amountOfYToMove);
         }
 
         /// <summary>
@@ -206,7 +235,8 @@ namespace AnimalSimulationVersion2
 
         public void Sleep()
         {
-            throw new NotImplementedException();
+            Sleeping = true;
+            TimeSlept = 0;
         }
     }
 }
