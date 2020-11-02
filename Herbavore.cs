74f972a2dea0210abe7618c9c0d87ed4c4478d2e
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-02 12:41:18 +0100

    altered FindFood() and FindMate() to return string instead of void. Also fixed an error in Herbavore ctor

--- Herbavore.cs
+++ Herbavore.cs
@@ -7,7 +7,7 @@ namespace AnimalSimulationVersion2
 {
     abstract class Herbavore : Animalia
     {
-        public Herbavore(string species, int reproductionAge, (float X, float Y) location, float maxAge, (byte Minimum, byte Maximum) birthAmount, float movementSpeed, float hunger, Point[] design, (int Red, int Green, int Blue) colour, string[] foodSource, float nutrienceValue, IHelper helper, AnimalPublisher animalPublisher, DrawdrawPublisher drawPublisher) : base(species, reproductionAge, location, maxAge, birthAmount, movementSpeed, hunger, design, colour, foodSource, nutrienceValue, helper, animalPublisher, drawPublisher)
+        public Herbavore(string species, int reproductionAge, (float X, float Y) location, float maxAge, (byte Minimum, byte Maximum) birthAmount, float movementSpeed, float hunger, Point[] design, (int Red, int Green, int Blue) colour, string[] foodSource, float nutrienceValue, IHelper helper, AnimalPublisher animalPublisher, DrawPublisher drawPublisher) : base(species, reproductionAge, location, maxAge, birthAmount, movementSpeed, hunger, design, colour, foodSource, nutrienceValue, helper, animalPublisher, drawPublisher)
         {
         }
 
@@ -17,13 +17,11 @@ namespace AnimalSimulationVersion2
 
         protected abstract override void Eat();
 
-        protected abstract override void FindFood();
+        protected abstract override string FindFood();
 
-        protected abstract override void FindMate();
+        protected abstract override string FindMate();
 
-        //protected abstract override char GenerateGender();
-
-        protected abstract override void Mating();
+        protected abstract override void Mate();
 
         protected abstract override void Move();
     }
