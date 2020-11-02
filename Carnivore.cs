bbc06edef290a1c01348c7dfb74dcdd8847502da
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-02 11:22:35 +0100

    added the new IHunt properties

--- Carnivore.cs
+++ Carnivore.cs
@@ -8,6 +8,8 @@ namespace AnimalSimulationVersion2
     abstract class Carnivore : Animalia, IHunt
     {
         public abstract float AttackRange { get; set; }
+        public abstract float AttackSpeedMultiplier { get; set; }
+        public (float X, float Y) PreyLastLocation { get; set; }
 
         public Carnivore(string species, int reproductionAge, (float X, float Y) location, float maxAge, (byte Minimum, byte Maximum) birthAmount, float movementSpeed, float hunger, Point[] design, (int Red, int Green, int Blue) colour, string[] foodSource, float nutrienceValue, IHelper helper, AnimalPublisher animalPublisher, DrawPublisher drawPublisher) : base(species, reproductionAge, location, maxAge, birthAmount, movementSpeed, hunger, design, colour, foodSource, nutrienceValue, helper, animalPublisher, drawPublisher)
         {
