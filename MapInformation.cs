edc5e2e81a7771c8ca55ad7829023064e3699fd2
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-05 13:02:50 +0100

    added a SetSeizeOfMap property and added a variable mapSize that both it and GetSizeOfMap use

--- MapInformation.cs
+++ MapInformation.cs
@@ -6,7 +6,9 @@ namespace AnimalSimulationVersion2
 {
     class MapInformation
     {
-        public (ushort width, ushort height) GetSizeOfMap { get;  }
+        public (ushort width, ushort height) mapSize;
+        public (ushort width, ushort height) GetSizeOfMap { get => mapSize;  }
+        public (ushort width, ushort height) SetSizeOfMap { set => mapSize = value; }
         public static MapInformation Instance { get; }
         static MapInformation() //consider a way to set the map size and any other possible information
         {
