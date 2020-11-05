23a1aeed7a5aac8ef8a35bcad9b223faf3d4f7c9
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-05 15:28:44 +0100

    trying to solve a problem

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Image = System.Windows.Controls.Image;
 
 namespace AnimalSimulationVersion2
 {
@@ -20,9 +24,54 @@ namespace AnimalSimulationVersion2
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static Image image;
         public MainWindow()
         {
             InitializeComponent();
+            image = ImageBox;
+            Loaded += OnWindowLoaded;
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            //create and run the visual thread and the AI thread.
+            MapInformation mapInformation = MapInformation.Instance;
+            mapInformation.SetSizeOfMap = ((ushort)image.Width, (ushort)image.Height);
+            Output output = Output.Instance;
+            output.Map = new Bitmap(mapInformation.GetSizeOfMap.width, mapInformation.GetSizeOfMap.height);
+            new Wolf("Carnis Lupus", (20, 20), null, Helper.Instance, Publisher.GetAnimalInstance, Publisher.GetDrawInstance, MapInformation.Instance); //here for testing and nothing else.
+            output.RunVisualThread();
+            output.RunAIThread();
+
+        }
+
+        public delegate void UpdateVisualDelegate(BitmapImage image);
+        public static UpdateVisualDelegate VisualUpdate = UpdateVisualImage;
+
+        private static void UpdateVisualImage(BitmapImage bitmapImage) //send this one with output using a delegate
+        { //this when called causes problems
+            //Exception: System.InvalidOperationException - The calling thread cannot access this object because a different thread owns it.
+            //this.Dispatcher.Invoke((Action)(() => { image.Source = bitmapImage; })); //does not work since it is a static class
+            //System.Windows.Threading.Dispatcher.CurrentDispatcher.Invoke((Action)(() => { image.Source = bitmapImage; })); //did not help
+            //System.Windows.Application.Current.Dispatcher.Invoke((Action)(() => { image.Source = bitmapImage; })); //nope 
+            image.Source = bitmapImage;
+        }
+        public delegate BitmapImage ConvertBitMapToBitMapIamge(Bitmap bitmap);
+        public static ConvertBitMapToBitMapIamge GenerateBitMapImage = ConvertBitmapToBitmapImage;
+        private static BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memoryStream = new MemoryStream()) //opens a memory stream, used to save an image in the stream and then load it into another type
+            {
+                bitmap.Save(memoryStream, ImageFormat.Bmp); //saves the bitmap to the stream
+                memoryStream.Position = 0;
+                bitmapImage.BeginInit();
+
+                bitmapImage.StreamSource = memoryStream; //loads the bitmap into a bitmapimage
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            return bitmapImage;
         }
     }
 }
--- Output.cs
+++ Output.cs
@@ -1,10 +1,77 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Text;
+using System.Threading;
+using System.Windows.Media.Imaging;
 
 namespace AnimalSimulationVersion2
 {
     class Output
     { //draws out to the map image and displays it
+        public Bitmap Map { get; set; }
+        public BitmapImage MapImage { get; set; }
+        public float FramesPerSecond { get; set; }
+        private float TimeInSecondsBetweenFrames { get; set; }
+        private DrawPublisher DrawPublisher { get; set; }
+
+        public static Output Instance { get; }
+
+        static Output()
+        {
+            Instance = new Output();
+            Instance.DrawPublisher = Publisher.GetDrawInstance;
+            Instance.FramesPerSecond = 30;
+            Instance.TimeInSecondsBetweenFrames = 1 / Instance.FramesPerSecond;
+        }
+        public void RunVisualThread()
+        {
+            Thread thread = new Thread(VisualThread);
+            thread.Start();
+        }
+        public void RunAIThread()
+        {
+        }
+
+        private void VisualThread()
+        {
+            //Thread.CurrentThread.IsBackground = false; //did not help
+            DateTime lastTime = DateTime.Now;
+            while (true)
+            {
+                if ((DateTime.Now-lastTime).TotalSeconds >= TimeInSecondsBetweenFrames) //the part before >= needs also be accessed by the AI thread. 
+                {
+                    List<(Point[] Design, (int Red, int Green, int Blue) Colour, (float X, float Y) Location)> drawInforamtion = DrawPublisher.Draw();
+                    Map = Draw(Map, drawInforamtion);
+
+                    lastTime = DateTime.Now;
+                    MapImage = MainWindow.GenerateBitMapImage(Map);
+                    MainWindow.VisualUpdate(MapImage);
+                }
+            }
+        }
+
+        private Bitmap Draw(Bitmap map, List<(Point[] Design, (int Red, int Green, int Blue) Colour, (float X, float Y) Location)> drawInforamtion)
+        {
+            using (Graphics g = Graphics.FromImage(map)) 
+                foreach ((Point[] Design, (int Red, int Green, int Blue) Colour, (float X, float Y) Location) information in drawInforamtion)
+                {
+                    //for now, consider Location as top left.
+                    for(int i = 0; i < information.Design.Length; i++)
+                    {
+                        information.Design[i].X += (int)information.Location.X;
+                        information.Design[i].Y += (int)information.Location.Y;
+                    }
+                    using (Pen pen = new Pen(Color.FromArgb(information.Colour.Red, information.Colour.Green, information.Colour.Blue), 1))
+                    {
+                        g.DrawPolygon(pen, information.Design);
+                    }
+
+                }
+            return map;
+        }
     }
 }
