e3e2cf2a9a3e5e2c6e4484a40a889ba16a8ccd8d
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-09 15:11:22 +0100

    added a comment

--- IPack.cs
+++ IPack.cs
@@ -11,7 +11,7 @@ namespace AnimalSimulationVersion2
         public abstract string[] PackMembers { get; set; }
         public abstract byte PackSize { get; set; }
         public abstract byte MaxPackSize { get; set; }
-        public abstract bool CanFightForAlpha { get; set; }
+        public abstract bool CanFightForAlpha { get; set; } //how to ensure the animals do not fight all the time and also figure out why one animal would fight another for alpha
         public abstract void FightAlpha(string alphaID); //delegate
         public abstract void FightChallenger(string challengerID); //delegate
     }
