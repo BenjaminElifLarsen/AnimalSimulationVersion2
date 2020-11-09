f684ba819c766bb4dcb69677f95ab7b0bd89a8e3
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-09 11:38:59 +0100

    MapInformation now contains a property for how many seconds an age is, OneAgeInSeconds

--- MapInformation.cs
+++ MapInformation.cs
@@ -9,10 +9,13 @@ namespace AnimalSimulationVersion2
         public (ushort width, ushort height) mapSize;
         public (ushort width, ushort height) GetSizeOfMap { get => mapSize;  }
         public (ushort width, ushort height) SetSizeOfMap { set => mapSize = value; }
+        public float OneAgeInSeconds { get; set; }
         public static MapInformation Instance { get; }
+        
         static MapInformation() //consider a way to set the map size and any other possible information
         {
             Instance = new MapInformation();
+            Instance.OneAgeInSeconds = 6;
         }
     }
 }
