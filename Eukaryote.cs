d20535121f0ffb75bb243a21aee825d4633a8a6e
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-11 12:08:11 +0100

    added properties, variables and methods that is shared between Plantae and Animalia with xml and the old implementations.

--- Eukaryote.cs
+++ Eukaryote.cs
@@ -7,29 +7,100 @@ namespace AnimalSimulationVersion2
 {
     abstract class Eukaryote
     {
+        /// <summary>
+        /// 
+        /// </summary>
         protected float timeAlive;
+        /// <summary>
+        /// 
+        /// </summary>
         protected float timeSinceLastUpdate;
+        /// <summary>
+        /// The amount of seconds since last update.
+        /// </summary>
         protected float reproductionCooldown;
-        protected float LengthOfReproduction;
-
-        protected MapInformation mapInformation;
+        /// <summary>
+        /// The length of the reproduction in seconds.
+        /// </summary>
+        protected float lengthOfReproduction;
+        /// <summary>
+        /// How long time the current reproduction has lasted in seconds.
+        /// </summary>
+        protected float periodInReproduction;
+        /// <summary>
+        /// 
+        /// </summary>
+        protected MapInformation mapInformation; //maybe have all variables as abstract, except those that is set in this class, properties so other will know to set them.
+        /// <summary>
+        /// 
+        /// </summary>
         protected IHelper helper;
+        /// <summary>
+        /// 
+        /// </summary>
         protected AnimalPublisher animalPublisher;
+        /// <summary>
+        /// 
+        /// </summary>
         protected DrawPublisher drawPublisher;
 
+        /// <summary>
+        /// The current age of the lifeform.
+        /// </summary>
         protected float Age { get; set; }
+        /// <summary>
+        /// The age when the animal can reproduce.
+        /// </summary>
         protected float ReproductionAge { get; set; }
+        /// <summary>
+        /// The maximum age of the lifeform.
+        /// </summary>
         protected float MaxAge { get; set; }
+        /// <summary>
+        /// The species of the lifeform.
+        /// </summary>
         protected string Species { get; set; }
+        /// <summary>
+        /// The current location of the lifeform.
+        /// </summary>
         protected (float X, float Y) Location { get; set; }
+        /// <summary>
+        /// The amount of time before the lifeform will feel a need to reproduce in seconds.
+        /// </summary>
         protected float TimeToReproductionNeed { get; set; }
+        /// <summary>
+        /// The design of the lifeform.
+        /// </summary>
         protected Point[] Design { get; set; }
+        /// <summary>
+        /// The RGB colour of the lifeform.
+        /// </summary>
         protected (byte Red, byte Green, byte Blue) Colour { get; set; }
+        /// <summary>
+        /// The unique ID of the lifeform.
+        /// </summary>
         protected string ID { get; set; }
+        /// <summary>
+        /// The health of the lifeform.
+        /// </summary>
         protected float Health { get; set; }
-        protected string[] HuntedBy { get; set; }
+        /// <summary>
+        /// Predatores of the lifeform.
+        /// </summary>
+        protected string[] HuntedBy { get; set; } //the IDs that are after it
+        /// <summary>
+        /// The nutrience value of the lifeform.
+        /// </summary>
         protected float NutrienValue { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
         protected float OneAgeInSeconds { get; set; }
+
+        /// <summary>
+        /// True if the lifeform has reproduced and is waiting on children.
+        /// </summary>
+        protected bool HasReproduced { get; set; } //find a better name
 
         public Eukaryote(string species, (float X, float Y) location, IHelper helper, AnimalPublisher animalPublisher, DrawPublisher drawPublisher, MapInformation mapInformation) : this(helper, animalPublisher, drawPublisher, mapInformation)
         {
@@ -58,34 +129,73 @@ namespace AnimalSimulationVersion2
 
             drawPublisher.RaiseDrawEvent += DrawEventHandler;
         }
+        /// <summary>
+        /// The 'AI' of the lifeform.
+        /// </summary>
         protected abstract void AI();
         protected virtual void TimeUpdate()
         {
-            throw new NotImplementedException();
+            timeAlive += timeSinceLastUpdate;
+            Age = timeAlive / OneAgeInSeconds;
+            TimeToReproductionNeed -= timeSinceLastUpdate;
+            if (periodInReproduction < lengthOfReproduction && HasReproduced)
+                periodInReproduction += timeSinceLastUpdate;
         }
         protected virtual void Death()
-        { //things that are shared between Plantae and Animalia could be moved up to an abstract class called Lifeform
+        { 
 
+            if (HuntedBy.Length != 0)
+            {
+                foreach (string hunterID in HuntedBy)
+                    animalPublisher.InformPredatorOfDeath(ID, hunterID);
+            }
+            RemoveSubscriptions();
         }
+        /// <summary>
+        /// Is asked for information such that another animal can decided if this animal is food or not.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected virtual void IsPossiblePreyEventHandler(object sender, ControlEvents.GetPossiblePreyEventArgs e)
-        {
-
+        { //delegate. Send back location, ID and species. 
+            if (e.SenderID != ID)
+            {
+                ((float X, float Y) PreyLocation, string PreyID, string PreySpeices) preyInformation = (Location, ID, Species);
+                e.AddPreyInformation(preyInformation);
+            }
         }
+        /// <summary>
+        /// Is informed that another animal is considering it food.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected virtual void IsPreyEventHandler(object sender, ControlEvents.SetPreyEventArgs e)
-        {
-
+        { //delegate. Take the ID of the predator and add it to the array. 
+            if (e.IDs.receiverID == ID)
+            {
+                string[] array = HuntedBy;
+                helper.Add(ref array, e.IDs.senderID);
+                HuntedBy = array;
+            }
         }
+        /// <summary>
+        /// Its predator is dead or have lost this animal.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected virtual void RemovePredatorEventHandler(object sender, ControlEvents.RemovePreyEventArgs e)
-        {
-
+        { //delegate. The predator has died or is lost to this animal. 
+            if (e.IDs.senderID != ID)
+                if (helper.Contains(HuntedBy, e.IDs.senderID))
+                    helper.Remove(HuntedBy, e.IDs.senderID);
         }
         protected virtual void LocationEventHandler(object sender, ControlEvents.GetOtherLocationEventArgs e)
-        {
+        { //delegate. Someone needs this one's location.
             if (e.ReceiverID == ID)
                 e.Location = Location;
         }
         protected virtual void EatenEventHandler(object sender, ControlEvents.EatenEventArgs e)
-        {
+        { //delegate. This animal has been eaten.
             if (e.ReceiverID == ID)
             {
                 e.SetNutrience(NutrienValue);
@@ -93,20 +203,30 @@ namespace AnimalSimulationVersion2
             }
         }
         protected virtual void DeathEventHandler(object sender, ControlEvents.DeadEventArgs e)
-        {
+        { //delegate. This animal has died. E.g. fought to death.
             if (e.ReceiverID == ID)
                 Death();
         }
+        /// <summary>
+        /// Asked to return information that permits the animal to be drawned.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected virtual void DrawEventHandler(object sender, ControlEvents.DrawEventArgs e)
-        {
+        { //delegate. Transmit location, design and colour back.
             if (Design != null)
             {
                 (Point[] Design, (byte Red, byte Green, byte Blue), (float X, float Y) Location) drawInforamtion = (helper.DeepCopy(Design), Colour, Location); //(type,type) will ac
                 e.AddDrawInformation(drawInforamtion);
             }
         }
+        /// <summary>
+        /// Asked to run a sequence of its AI.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected virtual void ControlEventHandler(object sender, ControlEvents.AIEventArgs e)
-        {
+        { //delgate
             timeSinceLastUpdate = e.TimeSinceLastUpdate;
             AI();
         }
