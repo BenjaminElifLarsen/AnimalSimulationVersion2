677e8190c51e20a687a4847731fc7e641cfe200d
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-11 12:51:57 +0100

    working on implementing basic plant methods, properties and variables

--- Plantae.cs
+++ Plantae.cs
@@ -5,27 +5,66 @@ using System.Text;
 
 namespace AnimalSimulationVersion2
 {
+    /// <summary>
+    /// Abstract class for all plants, by default methods assume any plants inheriting are monoecious.
+    /// </summary>
+    /// <remarks>
+    /// This means functions like void Polinate() and void Reproduce() needs to be overwritten for non-monoecious plants.
+    /// </remarks>
     abstract class Plantae : Eukaryote
     {
-        protected float lengthOfReproduction;
         protected float spreadRange;
-        protected bool HasPolinated { get; set; }
+        protected (byte Minimum, byte Maximum) offspringAmount;
 
         public Plantae(string species, (float X, float Y) location, IHelper helper, AnimalPublisher animalPublisher, DrawPublisher drawPublisher, MapInformation mapInformation) : base(species, location, helper, animalPublisher, drawPublisher, mapInformation)
         {
         }
+        /// <summary>
+        /// Basic implementation of AI.
+        /// </summary>
         protected override void AI()
         {
-
+            TimeUpdate();
+            if (TimeToReproductionNeed <= 0)
+                Polinate();
+            if (HasReproduced && periodInReproduction >= lengthOfReproduction)
+                Reproduce();
         }
 
+        /// <summary>
+        /// Baisc implementation of polinating the plant.
+        /// </summary>
         protected virtual void Polinate()
         {
-            throw new NotImplementedException();
+            TimeToReproductionNeed = reproductionCooldown + lengthOfReproduction;
+            periodInReproduction = 0;
+            HasReproduced = true;
         }
-        protected virtual void Reproduce()
+        /// <summary>
+        /// Basic implementation of reproduction, assumes the plant is monoecious
+        /// </summary>
+        protected virtual void Reproduce() //needs testing
         {
-            throw new NotImplementedException();
+            byte amountOfOffsprings = (byte)helper.GenerateRandomNumber(offspringAmount.Minimum, offspringAmount.Maximum);
+            Type type = this.GetType();
+            object[] dataObject = new object[6];
+            dataObject[0] = Species;
+            dataObject[2] = helper;
+            dataObject[3] = animalPublisher;
+            dataObject[4] = drawPublisher;
+            dataObject[5] = mapInformation;
+            for (byte i = 0; i <amountOfOffsprings; i++)
+            {
+                float xPercentage = (float)(helper.GenerateRandomNumber(0, 100) / 100f);
+                float xMaxDistance = xPercentage * spreadRange;
+                float yMaxDistance = (1 - xPercentage) * spreadRange; 
+                float xDistance = helper.GenerateRandomNumber(0, (int)xMaxDistance) - xMaxDistance / 2f;
+                float yDistance = helper.GenerateRandomNumber(0, (int)yMaxDistance) - yMaxDistance / 2f;
+                (float X, float Y) spawnLocation = (Location.X + xDistance, Location.Y + yDistance);
+                dataObject[1] = spawnLocation;
+                Activator.CreateInstance(type, dataObject);
+            }
+            HasReproduced = false;
         }
     }
 
