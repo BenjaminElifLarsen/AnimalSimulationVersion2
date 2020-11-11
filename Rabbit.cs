dc33d792d6738a1d63ee1c9a2d3846706b966244
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-11 09:41:58 +0100

    added more initialisation, updated the AI to allow reproduction and added code to GiveBirth() and Mate()

--- Rabbit.cs
+++ Rabbit.cs
@@ -13,16 +13,51 @@ namespace AnimalSimulationVersion2
             Colour = (0,120,120);
             Design = new Point[] { new Point(3,0), new Point(6, 6), new Point(0, 6) };
             NutrienValue = 100;
+
+            genderInformation = new (char Gender, byte Weight)[] { ('f', 50), ('m', 50) };
+            Gender = GenerateGender(genderInformation);
+            reproductionCooldown = 20;
+            BirthAmount = (2, 5);
+            ReproductionAge = 2;
+            lengthOfPregnacy = 6;
+
+            MaxHunger = 100;
+            Hunger = MaxHunger;
+            HungerFoodSeekingLevel = 0.5f;
         }
 
         public int StealthLevel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public override void AI()
         {
-            if (Location == MoveTo)
-                MoveTo = GenerateRandomEndLocation();
-            CurrentMovementSpeed = MovementSpeed;
-            Move();
+            TimeUpdate();
+            if (Gender == 'f')
+                if (HasMated)
+                    if (periodInPregnacy >= lengthOfPregnacy)
+                        GiveBirth();
+            if (Age >= ReproductionAge && TimeToReproductionNeed <= 0)
+            {
+                if (mateID == null)
+                    mateID = FindMate();
+                if (mateID != null)
+                {
+                    CurrentMovementSpeed = MovementSpeed;
+                    MoveTo = MateLocation = GetMateLocation(mateID);
+                    Move();
+                    Mate();
+                }
+                else
+                    DefaultMovement();
+            }
+            else
+                DefaultMovement();
+            void DefaultMovement()
+            {
+                if (Location == MoveTo)
+                    MoveTo = GenerateRandomEndLocation();
+                CurrentMovementSpeed = MovementSpeed;
+                Move();
+            }
         }
 
         public void HideFromPredator()
@@ -30,24 +65,31 @@ namespace AnimalSimulationVersion2
             throw new NotImplementedException();
         }
 
-        protected override string FindFood()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override string FindMate()
-        {
-            throw new NotImplementedException();
-        }
-
         protected override void GiveBirth()
         {
-            throw new NotImplementedException();
+            byte childAmount = (byte)helper.GenerateRandomNumber(BirthAmount.Minimum, BirthAmount.Maximum); 
+            for (int i = 0; i < childAmount; i++)
+                new Rabbit(Species, Location, FoodSource, helper, animalPublisher, drawPublisher, mapInformation);
+            //TimeToReproductionNeed = reproductionCooldown - periodInPregnacy; 
+            HasMated = false;
         }
 
         protected override void Mate()
         {
-            throw new NotImplementedException();
+            if (MateLocation == Location && !HasMated)
+            {
+                periodInPregnacy = 0;
+                if (Gender == 'f')
+                {
+                    HasMated = true;
+                    TimeToReproductionNeed = reproductionCooldown - periodInPregnacy;
+                }
+                else
+                {
+                    TimeToReproductionNeed = reproductionCooldown;
+                }
+                mateID = null;
+            }
         }
 
         protected override void Move()
