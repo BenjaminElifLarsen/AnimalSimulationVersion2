3c932777025365eb14edf715de7871d7cb80e621
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-11 09:47:41 +0100

    added some implementation to Plantae and added Eukaryote as an inherit

--- Plantae.cs
+++ Plantae.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AnimalSimulationVersion2
 {
-    class Plantae
+    abstract class Plantae : Eukaryote
     {
+        protected float lengthOfReproduction;
+        protected float spreadRange;
+        protected bool HasPolinated { get; set; }
+
+        public Plantae(string species, (float X, float Y) location, IHelper helper, AnimalPublisher animalPublisher, DrawPublisher drawPublisher, MapInformation mapInformation) : base(species, location, helper, animalPublisher, drawPublisher, mapInformation)
+        {
+        }
+        protected override void AI()
+        {
+
+        }
+
+        protected virtual void Polinate()
+        {
+            throw new NotImplementedException();
+        }
+        protected virtual void Reproduce()
+        {
+            throw new NotImplementedException();
+        }
     }
+
 }
