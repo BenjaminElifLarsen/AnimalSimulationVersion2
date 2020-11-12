f0a970e79b356693a947138bb61651d020685232
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-12 14:42:41 +0100

    altered Alphas and PackMembers to enum PackRelationshop. Added TimeBetweenFights and FightCooldown properties

--- IPack.cs
+++ IPack.cs
@@ -7,12 +7,20 @@ namespace AnimalSimulationVersion2
 {
     interface IPack //: IFamily
     {
-        public abstract string[] Alphas { get; set; }
-        public abstract string[] PackMembers { get; set; }
+        public enum PackRelationship
+        {
+            Alpha = 1,
+            Member = 2,
+            NonMember = 0
+        }
+        public abstract (PackRelationship Relationship, string ID)[] PackMembers { get; set; }
         public abstract byte PackSize { get; set; }
         public abstract byte MaxPackSize { get; set; }
+        public abstract float TimeBetweenFights { get; set; }
+        public abstract float FightCooldown { get; set; }
         public abstract bool CanFightForAlpha { get; set; } //how to ensure the animals do not fight all the time and also figure out why one animal would fight another for alpha
-        public abstract void FightAlpha(string alphaID); //delegate
-        public abstract void FightChallenger(string challengerID); //delegate
+        public abstract void FightAlpha(string alphaID); //transmit delegate. Have an event for doing health damage
+        public abstract void FightChallenger(string challengerID); //transmit delegate
+        
     }
 }
