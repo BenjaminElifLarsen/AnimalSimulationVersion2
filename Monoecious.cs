a30bafb8714e4491022fb196d06fc30baa334127
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-12 14:45:43 +0100

    added a death check in the AI and removed Reproduce()

--- Monoecious.cs
+++ Monoecious.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AnimalSimulationVersion2
+﻿namespace AnimalSimulationVersion2
 {
     class Monoecious : Plantae
     {
@@ -12,6 +8,8 @@ namespace AnimalSimulationVersion2
         protected override void AI()
         {
             TimeUpdate();
+            if (Age >= MaxAge || Health <= 0)
+                Death();
             if (TimeToReproductionNeed <= 0)
                 Polinate();
             if (HasReproduced && periodInReproduction >= lengthOfReproduction)
@@ -27,32 +25,7 @@ namespace AnimalSimulationVersion2
             periodInReproduction = 0;
             HasReproduced = true;
         }
-        /// <summary>
-        /// Basic implementation of reproduction, assumes the plant is monoecious
-        /// </summary>
-        protected override void Reproduce() //needs testing
-        {
-            byte amountOfOffsprings = (byte)helper.GenerateRandomNumber(offspringAmount.Minimum, offspringAmount.Maximum);
-            Type type = this.GetType();
-            object[] dataObject = new object[6];
-            dataObject[0] = Species;
-            dataObject[2] = helper;
-            dataObject[3] = animalPublisher;
-            dataObject[4] = drawPublisher;
-            dataObject[5] = mapInformation;
-            for (byte i = 0; i < amountOfOffsprings; i++)
-            {
-                float xPercentage = (float)(helper.GenerateRandomNumber(0, 100) / 100f);
-                float xMaxDistance = xPercentage * spreadRange;
-                float yMaxDistance = (1 - xPercentage) * spreadRange;
-                float xDistance = helper.GenerateRandomNumber(0, (int)xMaxDistance) - xMaxDistance / 2f;
-                float yDistance = helper.GenerateRandomNumber(0, (int)yMaxDistance) - yMaxDistance / 2f;
-                (float X, float Y) spawnLocation = (Location.X + xDistance, Location.Y + yDistance);
-                dataObject[1] = spawnLocation;
-                Activator.CreateInstance(type, dataObject);
-            }
-            HasReproduced = false;
-        }
+        
 
     }
 }
