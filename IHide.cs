ba4cbd609778f131d0b52735d59be631885ccf85
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-12 14:43:00 +0100

    added properties TimeHidden and MaxHideTime

--- IHide.cs
+++ IHide.cs
@@ -6,8 +6,9 @@ namespace AnimalSimulationVersion2
 {
     interface IHide //if it is hunted, for each predator generate a random number and if it is higher than a certian value that predator will "lose" the prey. The prey's movement speed is set to zero.
     { //maybe have a IFlee
-        public int StealthLevel { get; set; }
-
+        public int StealthLevel { get; set; } 
+        public float TimeHidden { get; set; }
+        public float MaxHideTime { get; set; }
         public void HideFromPredator(); //maybe old/young animals got a harder time to hide.
 
     }
