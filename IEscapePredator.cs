2313e86d106efb94e1dede1ba9c24ddaff30cd0f
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-12 14:44:25 +0100

    changed DiscoverChange To DiscoverChance and altered string[] to string in LostPredators

--- IEscapePredator.cs
+++ IEscapePredator.cs
@@ -6,10 +6,10 @@ namespace AnimalSimulationVersion2
 {
     interface IEscapePredator
     {
-        public (string[] ID, float TimeSinceEscape)[] LostPredators { get; set; } //used to prevent a predator from hunting the animal immediately
+        public (string ID, float TimeSinceEscape)[] LostPredators { get; set; } //used to prevent a predator from hunting the animal immediately
         public float EscapeSpeedMultiplier { get; set; }
         public float DiscoverRange { get; set; }
-        public byte DiscoverChange { get; set; }
+        public byte DiscoverChance { get; set; }
         public string PredatorID { get; set; }
         public float TimeThresholdForBeingHuntedAgain { get; set; } //1) rename. 2) time to compare to LostPredators to ensure it does not get hunted immediately by the same animal
         public float EscapeSprintTime { get; set; } //the amount of time the animal can run
