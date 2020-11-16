10b011807c1af879ac9e84d2f3fdd9b7f4a72807
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-16 15:04:17 +0100

    moved part of Reproduce() into GenerateChildren()

--- Plantae.cs
+++ Plantae.cs
@@ -20,6 +20,12 @@ namespace AnimalSimulationVersion2
         {
         }
 
+        protected override void AI()
+        {
+            if (Age >= MaxAge || Health <= 0)
+                Death();
+        }
+
         /// <summary>
         /// Baisc implementation of polinating the plant.
         /// </summary>
@@ -30,25 +36,37 @@ namespace AnimalSimulationVersion2
         protected override void Reproduce() //needs testing
         {
             byte amountOfOffsprings = (byte)helper.GenerateRandomNumber(offspringAmount.Minimum, offspringAmount.Maximum);
-            Type type = this.GetType();
             object[] dataObject = new object[6];
             dataObject[0] = Species;
             dataObject[2] = helper;
             dataObject[3] = animalPublisher;
             dataObject[4] = drawPublisher;
             dataObject[5] = mapInformation;
+            GenerateChildren(amountOfOffsprings, dataObject);
+            HasReproduced = false;
+        }
+        protected void GenerateChildren(int amountOfOffsprings, object[] objArray)
+        {
             for (byte i = 0; i < amountOfOffsprings; i++)
             {
+                Type type = this.GetType();
                 float xPercentage = (float)(helper.GenerateRandomNumber(0, 100) / 100f);
                 float xMaxDistance = xPercentage * spreadRange;
                 float yMaxDistance = (1 - xPercentage) * spreadRange;
                 float xDistance = helper.GenerateRandomNumber(0, (int)xMaxDistance) - xMaxDistance / 2f;
                 float yDistance = helper.GenerateRandomNumber(0, (int)yMaxDistance) - yMaxDistance / 2f;
                 (float X, float Y) spawnLocation = (Location.X + xDistance, Location.Y + yDistance);
-                dataObject[1] = spawnLocation;
-                Activator.CreateInstance(type, dataObject);
+                if (spawnLocation.X < 0)
+                    spawnLocation.X = 0;
+                else if (spawnLocation.X > mapInformation.mapSize.width)
+                    spawnLocation.X = mapInformation.mapSize.width - 1;
+                if (spawnLocation.Y < 0)
+                    spawnLocation.Y = 0;
+                else if (spawnLocation.Y > mapInformation.mapSize.height)
+                    spawnLocation.Y = mapInformation.mapSize.height - 1;
+                objArray[1] = new Vector(spawnLocation.X, spawnLocation.Y, 0);
+                Activator.CreateInstance(type, objArray);
             }
-            HasReproduced = false;
         }
     }
 }
