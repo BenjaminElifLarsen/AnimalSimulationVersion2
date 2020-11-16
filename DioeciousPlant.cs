88afc832ca7ea35550129ce8b6a80875068cfb70
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-11-16 15:08:04 +0100

    added two generic plant classes, one for each subtype

---
+++ DioeciousPlant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AnimalSimulationVersion2
+{
+    sealed class DioeciousPlant : Dioecious
+    {
+        public DioeciousPlant(string species, Vector location, IHelper helper, AnimalPublisher animalPublisher, DrawPublisher drawPublisher, MapInformation mapInformation, char gender = (char)0) : base(species, location, helper, animalPublisher, drawPublisher, mapInformation, gender)
+        {
+            spreadRange = 240;
+            offspringAmount = (2, 3);
+
+            lengthOfReproduction = 8;
+            reproductionCooldown = OneAgeInSeconds * 3;
+
+            NutrienValue = 40;
+
+            MaxHealth = 100;
+            Health = MaxHealth;
+            MaxAge = 7;
+            ReproductionAge = 3;
+            distanceDivider = 25;
+
+            Colour = (200, 100, 0);
+            Design = new Point[] { new Point(3, 0), new Point(6, 3), new Point(3, 6), new Point(0, 3) };
+
+        }
+    }
+}
---
+++ MonoeciousPlant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AnimalSimulationVersion2
+{
+    sealed class MonoeciousPlant : Monoecious //consider what extra stuff a melon can do that would not mate sense to have in Monoecious
+    { //after all, some plants reproduce through fire.
+        public MonoeciousPlant(string species, Vector location, IHelper helper, AnimalPublisher animalPublisher, DrawPublisher drawPublisher, MapInformation mapInformation) : base(species, location, helper, animalPublisher, drawPublisher, mapInformation)
+        {
+            spreadRange = 140;
+            offspringAmount = (1, 3);
+
+            lengthOfReproduction = 12;
+            reproductionCooldown = OneAgeInSeconds*2;
+
+            NutrienValue = 14;
+
+            MaxHealth = 100;
+            Health = MaxHealth;
+            MaxAge = 4;
+            ReproductionAge = 2;
+
+            Colour = (100, 200, 0);
+            Design = new Point[] { new Point(3,0), new Point(6,3), new Point(3,6), new Point(0,3) };
+
+        }
+    }
+}
